5f1f4380a58490e222da5f6d0a6bb843c388e349
konralitt <litwiniukkonrad01@gmail.com>
2026-07-05 01:41:11 +0200

    Initial Spring Boot project from Initializr

---
+++ src/test/java/io/github/KonradLitwiniuk/link_shortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.KonradLitwiniuk.link_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
