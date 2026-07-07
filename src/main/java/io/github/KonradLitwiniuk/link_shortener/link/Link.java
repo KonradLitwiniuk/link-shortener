d1382c555529edcf9cdd3cb1febdaeea3776c00b
konralitt <litwiniukkonrad01@gmail.com>
2026-07-07 21:30:33 +0200

    Add Link entity

---
+++ src/main/java/io/github/KonradLitwiniuk/link_shortener/link/Link.java
@@ -0,0 +1,25 @@
+package io.github.KonradLitwiniuk.link_shortener.link;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name="links")
+public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String url;
+    @Column(unique = true)
+    private String code;
+    private Instant createdAt;
+    public Link(String url, String code){
+        this.url = url;
+        this.code = code;
+        this.createdAt = Instant.now();
+    }
+}
+
